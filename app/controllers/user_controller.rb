a1491438484617fdd3ce95e22caed2de23fdf4ec
Sneha-jay <snehajpandiyan10@gmail.com>
2020-10-20 09:00:06 +0530

    Registration done

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,18 +1,19 @@
 class UserController < ApplicationController
   ActionController::Parameters.permit_all_parameters = true
   def index
+    @title = "RailsSpace User Hub"
   end
   private def user_params
-    params.require(:user).permit(:user)
+    params.require(:user).permit(:screen_name,:email,:password)
   end
   def register
     @title = "Register"
     if request.post? and params[:user]
         @user = User.new(user_params)
         if @user.save
-          render :text => "User created!"
+          flash[:notice] = "User #{@user.screen_name} created!"
+          redirect_to :action => "index"
         end
-        
+      end
     end
-end
-end
+  end
